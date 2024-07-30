e42104230745ea62f03e75ac9a33583f27e2a296
iamsukanta <sukantabalacste2016@gmail.com>
2024-07-30 23:32:00 +0600

    total count added

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import random
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
 
 def deposit():
   while True:
@@ -24,9 +30,29 @@ def get_number_of_lines():
     print("Please enter a number")
   return lines
 
+def get_bet():
+  while True:
+    amount = input("What would you like to bet on each line? $")
+    if amount.isdigit():
+      amount = int(amount)
+      if MIN_BET <= amount <= MAX_BET:
+        break
+      else:
+        print(f"Amount must be between {MIN_BET} - {MAX_BET}.")
+    print("Please enter a number")
+  return amount
+
 def main():
   balance = deposit()
   lines = get_number_of_lines()
-  print(balance, lines)
+  while True:
+    bet = get_bet()
+    total_bet = bet*lines
+    if total_bet > balance:
+      print(f"You do not have enough amount to bet on that amount, your current balance is: ${balance}")
+    else:
+      break
+
+  print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
 
 main()
