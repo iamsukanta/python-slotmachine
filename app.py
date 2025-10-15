8b1d3885b5f0da2a23710593ae66bf9ea872b7f5
iamsukanta <sukantabalacste2016@gmail.com>
2025-10-15 20:17:37 +0200

    lambda function added

--- app.py
+++ app.py
@@ -39,13 +39,13 @@
 
 # List Comprehension
 
-list1 = [1, 2, 3, 4, 5]
-list2 = [x**2 for x in list1 if x%2 == 0]
-print(list2)  # [4, 8]
+# list1 = [1, 2, 3, 4, 5]
+# list2 = [x**2 for x in list1 if x%2 == 0]
+# print(list2)  # [4, 8]
 
-list = ['apple', 'banana', 'cherry']
-new_list = [x for x in list if x != 'banana']
-print(new_list)
+# list = ['apple', 'banana', 'cherry']
+# new_list = [x for x in list if x != 'banana']
+# print(new_list)
 
 #  Deep copy vs Shallow copy
 
@@ -57,4 +57,23 @@ deep_copied = copy.deepcopy(original)
 original[0][0] = 99
 print(original)         # [[99, 2, 3], [4, 5, 6]]
 print(shallow_copied)  # [[99, 2, 3], [4, 5, 6]]
-print(deep_copied)     # [[1, 2, 3], [
+print(deep_copied)     # [[1, 2, 3], [4, 5, 6]]
+
+# Lambda Function
+
+x = lambda a : a+10
+print(x(5))  # 15
+
+y = lambda a, b,c  : a if a > b and a>c else b if b>c else c
+print(y(2, 10, 3))  # 10
+
+list1 = [5, 2, 9, 1]
+new_list = list(map(lambda x: x*2, list1))
+print(new_list)  # [10, 4, 18, 2]
+print(list1)  # [5, 2, 9, 1]
+
+list4 = [5, 2, 9, 1]
+def square(x):
+    return x*x
+squared_list = list(map(square, list1))
+print(squared_list)  # [25, 4, 81, 1]
